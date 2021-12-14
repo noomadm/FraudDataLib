58619465fe9325a24dac5346e1bfeb1572d5763a
Kittisak Pukham <noomadm@gmail.com>
2021-12-14 15:21:11 +0700

    Watch List api

--- src/main/java/th/co/ktb/fraud/monitas/repositories/WatchListRepository.java
+++ src/main/java/th/co/ktb/fraud/monitas/repositories/WatchListRepository.java
@@ -1,6 +1,10 @@
 package th.co.ktb.fraud.monitas.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import th.co.ktb.fraud.monitas.models.WatchList;
@@ -8,6 +12,7 @@ import th.co.ktb.fraud.monitas.models.WatchList;
 @Repository
 public interface WatchListRepository extends CrudRepository<WatchList, Integer>{
 
-	
+	@Query("select wl from WatchList wl where :keyword = :keyword")
+	public Page<WatchList> search(@Param("keyword") String keyword,Pageable pagable);
 	
 }
